67f38ba9b61261ed84b5ba280db6b84902db88c9
Eriol G <eriol@flexie.io>
2021-12-19 19:04:46 +0100

    Added VAT yes/no in the config

--- components/settings/config/ChangeConfig.js
+++ components/settings/config/ChangeConfig.js
@@ -112,7 +112,130 @@ const FlexieFormWrapper = styled.div`
   input:read-only,
   input:disabled {
     background-color: ${(props) => props.theme.color.text.disabled};
-    opacity: 0.8;
+  }
+
+  .flexieForm-component-checkbox {
+    margin-bottom: 15px;
+  }
+
+  .flexieForm-component-checkbox label {
+    display: block;
+    margin-bottom: 5px;
+  }
+
+  .flexieForm-component-checkbox .fx-checkbox-inline {
+    display: inline-block;
+    padding-right: 20px;
+    margin-bottom: 0;
+    vertical-align: middle;
+  }
+  .flexieForm-component-checkbox .fx-checkbox {
+    --background: #fff;
+    --border: #d1d6ee;
+    --border-hover: #bbc1e1;
+    --border-active: #0099e5;
+    --tick: #fff;
+    position: relative;
+  }
+  .flexieForm-component-checkbox .fx-checkbox input,
+  .flexieForm-component-checkbox .fx-checkbox svg {
+    width: 18px;
+    height: 18px;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .flexieForm-component-checkbox .fx-checkbox span {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .flexieForm-component-checkbox .fx-checkbox input[type="radio"] {
+    border-radius: 50% !important;
+  }
+  .flexieForm-component-checkbox .fx-checkbox input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    position: relative;
+    outline: none;
+    background: var(--background);
+    border: none;
+    margin: 0 8px 0 0;
+    padding: 0;
+    cursor: pointer;
+    border-radius: 2px;
+    transition: box-shadow 0.3s;
+    box-shadow: inset 0 0 0 var(--s, 1px) var(--b, var(--border));
+  }
+  .flexieForm-component-checkbox .fx-checkbox input:hover {
+    --s: 3px;
+    --b: var(--border-hover);
+  }
+  .flexieForm-component-checkbox .fx-checkbox input:checked {
+    --b: var(--border-active);
+  }
+  .flexieForm-component-checkbox .fx-checkbox svg {
+    pointer-events: none;
+    fill: none;
+    stroke-width: 2px;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke: var(--stroke, var(--border-active));
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 18px;
+    height: 18px;
+    transform: scale(var(--scale, 1)) translateZ(0);
+  }
+  .flexieForm-component-checkbox .fx-checkbox.path input:checked {
+    --s: 2px;
+    transition-delay: 0.4s;
+  }
+  .flexieForm-component-checkbox .fx-checkbox.path input:checked + svg {
+    --a: 16.1 86.12;
+    --o: 102.22;
+  }
+  .flexieForm-component-checkbox .fx-checkbox.path svg {
+    stroke-dasharray: var(--a, 86.12);
+    stroke-dashoffset: var(--o, 86.12);
+    transition: stroke-dasharray 0.6s, stroke-dashoffset 0.6s;
+  }
+  .flexieForm-component-checkbox .fx-checkbox.check-bounce {
+    --stroke: var(--tick);
+  }
+  .flexieForm-component-checkbox .fx-checkbox.check-bounce input:checked {
+    --s: 11px;
+  }
+  .flexieForm-component-checkbox .fx-checkbox.check-bounce input:checked + svg {
+    -webkit-animation: check-bounce 0.4s linear forwards 0.2s;
+    animation: check-bounce 0.4s linear forwards 0.2s;
+  }
+  .flexieForm-component-checkbox .fx-checkbox.check-bounce svg {
+    --scale: 0;
+  }
+
+  @-webkit-keyframes check-bounce {
+    50% {
+      transform: scale(1.2);
+    }
+    75% {
+      transform: scale(0.9);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes check-bounce {
+    50% {
+      transform: scale(1.2);
+    }
+    75% {
+      transform: scale(0.9);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 
   .flexieForm-component-file .fa-window-close {
