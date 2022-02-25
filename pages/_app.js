920d1deea32a0de3ea4f768b8c4d3d59969d50ae
Eriol G <eriol@flexie.io>
2022-02-25 15:40:20 +0100

    Added progress bar on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
+import Router from "next/router";
+
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 // import { ReactQueryDevtools } from "react-query/devtools";
 import Head from "next/head";
-
 import Layout from "@components/layout/Layout";
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#7c5dfa",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 const queryClient = new QueryClient();
 
